2373cbb103cb0addb74eb9f329366f023ff2605a
unknown <janee2janee8090@gmail.com>
2025-11-21 16:54:45 +0530

    navbarcode

--- src/component/Home3.js
+++ src/component/Home3.js
@@ -1,20 +1,25 @@
-<<<<<<< HEAD
-import React from 'react'
+import React from "react";
+import styles from "../styles/Home3.module.css";
 
 function Home3() {
   return (
-       <>
+    <>
+    <div className={styles.hero}>
+      <div className={styles.right}>
+        <h1 className={styles.title}>
+         More than just a game<br></br>It’s a lifestyle. 
+        </h1>
+         <p className={styles.subtitle}>
+         Whether you’re just starting out,have<br></br>played your whole life or you're a<br></br>
+          Tour pro, your swing is like a fingerprint.
+        </p>
+        <button className={styles.btn}>Shopping Now</button>
+      </div>
+    </div>
+    <div className={styles.hero1}>
        
-       </>  
-  )
+    </div>
+    </>
+  );
 }
 export default Home3;
-=======
-import React from 'react'
-
-export default function Home3() {
-  return (
-    <div>Home3</div>
-  )
-}
->>>>>>> 50c7404e91b4ce8f4de8cc68873f6c8434b304d1
