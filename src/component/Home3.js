427ca48e2f3bcbfadf8e68fec2457712a9a4ca4a
unknown <janee2janee8090@gmail.com>
2025-11-21 19:41:15 +0530

    home321/11

--- src/component/Home3.js
+++ src/component/Home3.js
@@ -2,23 +2,31 @@ import React from "react";
 import styles from "../styles/Home3.module.css";
 
 function Home3() {
+  const part=[
+    {title:"Featured"}
+  ]
   return (
     <>
     <div className={styles.hero}>
       <div className={styles.right}>
-        <h1 className={styles.title}>
-         More than just a game<br></br>It’s a lifestyle. 
-        </h1>
+        <p className={styles.title}>
+         <span>More than</span> just a game<br></br>It’s a lifestyle. 
+        </p>
          <p className={styles.subtitle}>
-         Whether you’re just starting out,have<br></br>played your whole life or you're a<br></br>
-          Tour pro, your swing is like a fingerprint.
+         Whether you’re just starting out,have<br></br>played your whole life or you're a
+          Tour<br></br> pro, your swing is like a fingerprint.
         </p>
         <button className={styles.btn}>Shopping Now</button>
       </div>
     </div>
-    <div className={styles.hero1}>
-       
-    </div>
+    {part.map((parts,index)=>(
+      <div key={index} className={styles.hero1}>
+         <p>{parts.title}</p>
+        <div className={styles.title}>
+        </div>
+      </div>
+
+    ))}
     </>
   );
 }
