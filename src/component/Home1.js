2a549229eee9e713a43d2c343788062d7a5c01f9
egaajith <egaajith4343@gmail.com>
2025-11-25 00:25:38 +0530

    home125/11

--- src/component/Home1.js
+++ src/component/Home1.js
@@ -8,6 +8,15 @@ import homeproduct1 from "../img1/e-h3.png";
 import homeproduct2 from "../img1/e-h4.png";
 import homeproduct3 from "../img1/e-h5.png";
 
+import productcard1 from "../img1/card/1.png";
+import productcard2 from "../img1/card/2.png";
+import productcard3 from "../img1/card/3.png";
+import productcard4 from "../img1/card/4.png";
+
+import {
+  FaStar,
+  FaStarHalfAlt,
+} from "react-icons/fa";
 function Home1() {
   return (
     <>
@@ -84,49 +93,111 @@ function Homeslide() {
       </div>
 
       {/* --------- home Living Room--------- */}
-      {/* <div className={`container ${styles.home_product_container}`}>
-        <div className={`row ${styles.home_product_row}`}>
-          <div className={`col-md-6 ${styles.home_product_col1}`}>
-            <img src={homeproduct1} alt="product" />
-            <h4>Living Room</h4>
-            <p>Shop Now</p>
-          </div>
-          <div className={`col-md-6 ${styles.home_product_col2}`}>
-          <div className={`col-md-6 ${styles.home_product_col21}`}>
-            <img src={homeproduct2} alt="product" />
-            <h4>Living Room</h4>
-            <p>Shop Now</p>
-          </div>
-          <div className={`col-md-6 ${styles.home_product_col22}`}>
-            <img src={homeproduct3} alt="product" />
-            <h4>Living Room</h4>
-            <p>Shop Now</p>
-          </div>
-          </div>
-        </div>
-      </div> */}
-
       <div className={`container ${styles.home_product_container}`}>
         <div className={styles.home_product_row}>
           <div className={styles.home_product_col1}>
             <h4>Living Room</h4>
-            <p>Shop Now →</p>
+            <a href="/shop">
+              <p>Shop Now →</p>{" "}
+            </a>
             <img src={homeproduct1} alt="product" />
           </div>
           <div className={styles.home_product_col2}>
             <div className={styles.home_product_col21}>
               <h4>Bedroom</h4>
-              <p>Shop Now →</p>
+              <a href="/shop">
+                <p>Shop Now →</p>{" "}
+              </a>
               <img width={350} src={homeproduct2} alt="product" />
             </div>
             <div className={styles.home_product_col22}>
               <h4>Kitchen</h4>
-              <p>Shop Now →</p>
+              <a href="/shop">
+                <p>Shop Now →</p>{" "}
+              </a>
               <img width={350} src={homeproduct3} alt="product" />
             </div>
           </div>
         </div>
       </div>
+
+      {/* =======New Arrivals--------------------------------------- */}
+      <div className={styles.new_arrivals}>
+        <div className={styles.new1}>
+          <h1>
+            New <br />
+            Arrivals
+          </h1>
+          <a href="/shop">
+            <p>More Products →</p>
+          </a>
+        </div>
+        <div className={styles.cardwrap}>
+          <Cardhome />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Cardhome() {
+  const homecard = [
+    {
+      id: 1,
+      title: "Bamboo basket",
+      img: productcard1,
+      newprice: 499,
+      oldprice: 799,
+    },
+    {
+      id: 2,
+      title: "Beige Table Lamp",
+      img: productcard2,
+      newprice: 499,
+      oldprice: 799,
+    },
+    {
+      id: 3,
+      title: "Loveseat Sofa",
+      img: productcard3,
+      newprice: 499,
+      oldprice: 799,
+    },
+    {
+      id: 4,
+      title: "Table Lamp",
+      img: productcard4,
+      newprice: 499,
+      oldprice: 799,
+    },
+     {
+      id: 4,
+      title: "Table Lamp",
+      img: productcard1,
+      newprice: 499,
+      oldprice: 799,
+    },
+  ];
+  return (
+    <>
+      {homecard.map((product) => (
+        <div className={styles.card} key={product.id}>
+          <img src={product.img} alt="" className={styles.cardimg} />
+          <div className={styles.cardBody}>
+            <div className={styles.rating}>
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStarHalfAlt />
+            </div>
+            <h3 className={styles.title}>{product.title}</h3>
+            <div className={styles.price}>
+              <span>₹{product.newprice}</span>
+              <del>₹{product.oldprice}</del>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
