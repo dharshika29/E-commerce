1585d0a1e4344ae65bdc7993e359a2606154fb98
unknown <janee2janee8090@gmail.com>
2025-11-24 21:30:58 +0530

    home324/11

--- src/component/Home3.js
+++ src/component/Home3.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Home3.module.css";
 import featuredimg1 from '../image3/j-featuredimg1.png';
-import featuredimg2 from '../image3/j featuredimg2.png';
-import featuredimg3 from '../image3/j featuredimg3.png';
+import featuredimg2 from '../image3/j category4.png';
+import featuredimg3 from '../image3/j category6.png';
 import featuredimg4 from '../image3/j featured4.png';
 import { FaStar } from "react-icons/fa";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
-import category1 from '../image3/j categories1.jpg'
-import category2 from '../image3/j categories2.jpg'
-import category3 from '../image3/j categories3.jpg'
-import category4 from '../image3/j featuredimg2.png'
-import category5 from '../image3/j categories5.png'
-import category6 from '../image3/j featuredimg3.png'
+import category1 from '../image3/j category1.png'
+import category2 from '../image3/j category2.png'
+import category3 from '../image3/j category3.png'
+import category4 from '../image3/j category4.png'
+import category5 from '../image3/j category5.png'
+import category6 from '../image3/j category6.png'
+import limited from '../image3/j limitedimg.jpg'
+import bag1 from '../image3/j collectionbig.png'
+import bag2 from '../image3/j collectionsmal1.png'
+import bag3 from '../image3/j collectionsmal2.png'
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import article1 from '../image3/j article1.png'
+import article2 from '../image3/j article2.png'
+import article3 from '../image3/j article3.png'
+import { FaArrowRightLong } from "react-icons/fa6";
+
+
 
 function Home3() {
   const carditem = [
@@ -23,7 +34,44 @@ function Home3() {
   ]
   const part = [
     { img: category1, desc: "Golf Clubs" }, { img: category2, desc: "Golf Balls" }, { img: category3, desc: "Golf Bags" },
-    { img1: category4, desc: "Golf Clubs" }, { img1: category5, desc: "Golf Balls" }, { img1: category6, desc: "Golf Bags" },
+  ]
+  const part1 = [
+    { img1: category4, desc: "Clothing and RainWear" }, { img1: category5, desc: "FootWear" }, { img1: category6, desc: "Accessories" },
+  ]
+  const [timeLeft, setTimeLeft] = useState({
+    days: 2,
+    hours: 12,
+    minutes: 45,
+    seconds: 5,
+  });
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => {
+        let { days, hours, minutes, seconds } = prev;
+
+        if (seconds > 0) seconds--;
+        else {
+          seconds = 59;
+          if (minutes > 0) minutes--;
+          else {
+            minutes = 59;
+            if (hours > 0) hours--;
+            else {
+              hours = 23;
+              if (days > 0) days--;
+            }
+          }
+        }
+
+        return { days, hours, minutes, seconds };
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+  const article=[
+    {img:article1,desc:"Air Jordan x Travis Scott Event"}, {img:article2,desc:"The timeless classics on the green"}, 
+    {img:article3,desc:"The 2023 Ryder Cup"}
   ]
   return (
     <>
@@ -71,16 +119,109 @@ function Home3() {
           </div>
         ))}
       </div>
-        <h1 className={styles.hh2}>Shop by Categories</h1>
-        <div className={styles.main}>
-         {part.map((parts,index)=>(
-          <div className={styles.category} key={index}>
-             <img src={parts.img} alt="" width="50%"/>
+      <h1 className={styles.hh2}>Shop by Categories</h1>
+      <div className={styles.main1}>
+        {part.map((parts) => (
+          <div className={styles.category}>
+            <img src={parts.img} alt="" width="100%" />
+            <p className={styles.pp3}>{parts.desc}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.main1}>
+        {part1.map((parts) => (
+          <div className={styles.category}>
+            <img src={parts.img1} alt="" width="100%" />
+            <p className={styles.pp3}>{parts.desc}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.main2}>
+        <img src={limited} className={styles.limitedimg} alt="" />
+        <div className={styles.Boxtwo}>
+          <p className={styles.edition}>LIMITED EDITION</p>
+          <h2 className={styles.offer}>Hurry up! 40% OFF</h2>
+          <p className={styles.subTitle}>Thousands of high tech are waiting for you</p>
+          <p className={styles.expire}>Offer expires in:</p>
+          <div className={styles.timerRow}>
+            <div className={styles.timerBox}>
+              <h3>{String(timeLeft.days).padStart(2, "0")}</h3>
+              <p>Days</p>
             </div>
-         ))}
+
+            <div className={styles.timerBox}>
+              <h3>{String(timeLeft.hours).padStart(2, "0")}</h3>
+              <p>Hours</p>
+            </div>
+
+            <div className={styles.timerBox}>
+              <h3>{String(timeLeft.minutes).padStart(2, "0")}</h3>
+              <p>Minutes</p>
+            </div>
+
+            <div className={styles.timerBox}>
+              <h3>{String(timeLeft.seconds).padStart(2, "0")}</h3>
+              <p>Seconds</p>
+            </div>
+          </div>
+
+          <button className={styles.btn}>Shop now</button>
         </div>
-      
-    
+      </div>
+      <h1 className={styles.hh1}>Shop Collection</h1>
+      <div className={styles.hero3}>
+        <div className={styles.grid}>
+
+          <div className={styles.bigCard}>
+            <img
+              src={bag1}
+              alt=""
+              className={styles.bigImage}
+            />
+            <div className={styles.Box}>
+              <h3>Junior's Set</h3>
+              <button className={styles.link}>Collection <MdKeyboardDoubleArrowRight /></button>
+            </div>
+          </div>
+          <div className={styles.smallCard}>
+            <img
+              src={bag2}
+              alt=""
+              className={styles.smallImage}
+            />
+            <div className={styles.textBox}>
+              <h3>Men's Set</h3>
+              <button className={styles.link}>Collection <MdKeyboardDoubleArrowRight /></button>
+            </div>
+          </div>
+          <div className={styles.smallCard}>
+            <img
+              src={bag3}
+              alt=""
+              className={styles.smallImage}
+            />
+            <div className={styles.textBox}>
+              <h3>Women's Set</h3>
+              <button className={styles.link}>Collection <MdKeyboardDoubleArrowRight /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className={styles.hh1}>Latest Articles</h1>
+        <div className={styles.main1}>
+        {article.map((articles) => (
+          <div className={styles.category}>
+            <img src={articles.img} alt="" width="100%" />
+            <p className={styles.pp4}>{articles.desc}</p>
+            <a href="">Read More <FaArrowRightLong /></a>
+          </div>
+        ))}
+      </div>
+      <h1 className={styles.hh2}>Join Our Newsletter</h1>
+      <p className={styles.hh2}>Sign up for deals, new products and promotions</p>
+
+
+
     </>
   );
 }
