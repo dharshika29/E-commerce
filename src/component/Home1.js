114f44261a562c1d9ff700c38b2bd6f5b9484c75
egaajith <egaajith4343@gmail.com>
2025-11-24 13:52:26 +0530

    home124/11

--- src/component/Home1.js
+++ src/component/Home1.js
@@ -4,6 +4,10 @@ import styles from "../styles/eHome1.module.css";
 import home1e from "../img1/e-h1.png";
 import home2e from "../img1/e-h2.jpg";
 
+import homeproduct1 from "../img1/e-h3.png";
+import homeproduct2 from "../img1/e-h4.png";
+import homeproduct3 from "../img1/e-h5.png";
+
 function Home1() {
   return (
     <>
@@ -54,7 +58,6 @@ function Homeslide() {
           } ${styles[fadeState]}`}
         >
           {/* slide img--------------------------------------------  */}
-
           <div
             className={` ${styles.slideimg} ${
               hslide.id === 1 ? styles.slidesimg1 : styles.slideimg2
@@ -74,9 +77,54 @@ function Homeslide() {
         </div>
         <div className={styles.bottom_text2}>
           <p>
-            <span>3legant</span> is a gift & decorations store based in HCMC, <br /> Vietnam.
-            Est since 2019.
+            <span>3legant</span> is a gift & decorations store based in HCMC,{" "}
+            <br /> Vietnam. Est since 2019.
           </p>
+        </div>
+      </div>
+
+      {/* --------- home Living Room--------- */}
+      {/* <div className={`container ${styles.home_product_container}`}>
+        <div className={`row ${styles.home_product_row}`}>
+          <div className={`col-md-6 ${styles.home_product_col1}`}>
+            <img src={homeproduct1} alt="product" />
+            <h4>Living Room</h4>
+            <p>Shop Now</p>
+          </div>
+          <div className={`col-md-6 ${styles.home_product_col2}`}>
+          <div className={`col-md-6 ${styles.home_product_col21}`}>
+            <img src={homeproduct2} alt="product" />
+            <h4>Living Room</h4>
+            <p>Shop Now</p>
+          </div>
+          <div className={`col-md-6 ${styles.home_product_col22}`}>
+            <img src={homeproduct3} alt="product" />
+            <h4>Living Room</h4>
+            <p>Shop Now</p>
+          </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className={`container ${styles.home_product_container}`}>
+        <div className={styles.home_product_row}>
+          <div className={styles.home_product_col1}>
+            <h4>Living Room</h4>
+            <p>Shop Now →</p>
+            <img src={homeproduct1} alt="product" />
+          </div>
+          <div className={styles.home_product_col2}>
+            <div className={styles.home_product_col21}>
+              <h4>Bedroom</h4>
+              <p>Shop Now →</p>
+              <img width={350} src={homeproduct2} alt="product" />
+            </div>
+            <div className={styles.home_product_col22}>
+              <h4>Kitchen</h4>
+              <p>Shop Now →</p>
+              <img width={350} src={homeproduct3} alt="product" />
+            </div>
+          </div>
         </div>
       </div>
     </>
